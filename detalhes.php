1195ce5b700987656b83afe0044e67664a77d313
Ana Paula <ap.almeidapm@gmail.com>
2022-10-27 17:41:00 +0100

    modificacoes na pagina index(exluir,editar) e detalhes(HTML)

--- detalhes.php
+++ detalhes.php
@@ -1,17 +1,26 @@
-<h1>Detalhe da Tarefa</h1>
 <?php
    include("db/config.php");
    $result=$conn->query("select * from tarefas where idtarefas=".$_GET["id"]);
-   while( $registro = $result->fetch_assoc()){
-      // echo $registro["idtarefas"];
-      echo " ";
-      echo $registro["titulo"];
-      echo $registro["data"];
-      echo $registro["hora"];
-      echo $registro["idcategoriaFK"];
-      echo $registro["descricao"];
-      echo"<br>";
-  }
-  
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <?php while( $registro = $result->fetch_assoc()){?>
+   <h1>Detalhes da Tarefa</h1>
+   <p><strong>Título da tarefa:</strong> <?=$registro["titulo"]?></p>
+   <p><strong>Data:</strong> <?=$registro["data"]?></p>
+   <p><strong>Hora:</strong> <?=$registro["hora"]?></p>
+   <p><strong>ID da categoria:</strong> <?=$registro["idcategoriaFK"]?></p>
+   <p><strong>Descrição:</strong> <?=$registro["descricao"]?></p>
+   <?php } ?>
+
+</body>
+</html>
+
+
--- index.php
+++ index.php
@@ -9,10 +9,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ToDo List - Home</title>
+    <style>
+        tr{
+            text-align:center;
+        }
+    </style>
 </head>
 <body>
-    <div>
+    <div class="container">
         <h1>ToDo List</h1>
+        <a href="cadastrar-tarefa.php"><button>Cadastrar Tarefas</button></a>
+        <br><br>
         <table border="1">
             <tr>
                 <td>Título</td>
@@ -20,6 +27,7 @@
                 <td>Hora</td>
                 <td>Categoria</td>
                 <td>Descrição</td>
+                <td>Opções</td>
             </tr>
             <?php foreach($result as $tarefa){ ?>
             <tr>
@@ -30,11 +38,14 @@
                 <td><?=$tarefa["hora"]?></td>
                 <td><?=$tarefa["idcategoriaFK"]?></td>
                 <td><?=$tarefa["descricao"]?></td>
+                <td>
+                    <a href="">Excluir</a>
+                    <a href="">Editar</a>
+                </td>
             </tr>
             <?php } ?>
         </table>
-        <br>
-        <a href="cadastrar-tarefa.php"><button>Cadastrar Tarefas</button></a>
+        
     </div>
 
   
